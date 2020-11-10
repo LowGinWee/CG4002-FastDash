f02f1cb1452533637afe966a7225e187af829954
LowGinWee <notginwee@gmail.com>
2020-11-10 17:15:52 +0800

    publish

--- mqttDashBoard.js
+++ mqttDashBoard.js
@@ -1,37 +1,70 @@
-// Create a client instance
-client = new Paho.MQTT.Client("broker.hivemq.com", Number(8000), "G17_123");
-
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-
-// connect the client
-client.connect({onSuccess:onConnect});
-
-var count = -1;
 
 
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("ChiHenIsASexyBoi/1");
-  message = new Paho.MQTT.Message("DASHBOARD CONNECTED! DANCE TO START");
-  message.destinationName = "ChiHenIsASexyBoi/1";
-  client.send(message);
+const CHANNEL_ID = 'UscnU30egG9jGkgR';
+count = -1
+const drone = new ScaleDrone(CHANNEL_ID, {
+ data: { // Will be sent out as clientData via events
+   name: getRandomName(),
+   color: getRandomColor(),
+ },
+});
+
+function getRandomName() {
+ const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+ const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+ return (
+   adjs[Math.floor(Math.random() * adjs.length)] +
+   "_" +
+   nouns[Math.floor(Math.random() * nouns.length)]
+ );
 }
 
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
+function getRandomColor() {
+ return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 
-// called when a message arrives
-function onMessageArrived(message) {
-	count = count + 1;
-  console.log("onMessageArrived:"+message.payloadString);
-  document.getElementById("Main").innerHTML = message.payloadString;
-  document.getElementById("DanceNum").innerHTML = count;
-}
+drone.on('open', error => {
+ if (error) {
+   return console.error(error);
+ }
+ console.log('Successfully connected to Scaledrone');
+
+ const room = drone.subscribe('observable-room');
+ room.on('open', error => {
+   if (error) {
+     return console.error(error);
+   }
+   console.log('Successfully joined room');
+ });
+
+ // Put this code into the drone.on('open') block
+ // right after the room 'open' listener
+
+ // List of currently online members, emitted once
+ room.on('members', m => {
+  members = m;
+  // updateMembersDOM(); uncomment later
+ });
+
+ // User joined the room
+ room.on('member_join', member => {
+  members.push(member);
+  // updateMembersDOM(); uncomment later
+ });
+
+ // User left the room
+ room.on('member_leave', ({id}) => {
+  const index = members.findIndex(member => member.id === id);
+  members.splice(index, 1);
+  // updateMembersDOM(); uncomment later
+ });
+
+ // Add this after 'member_leave' event
+ room.on('data', (text, member) => {
+   count = count + 1;
+   console.log("onMessageArrived:"+text);
+   document.getElementById("Main").innerHTML =text;
+   document.getElementById("DanceNum").innerHTML = count;
+ });
+
+});
